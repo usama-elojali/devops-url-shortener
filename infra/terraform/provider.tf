4e8b7d4fa74da86e9b71c4286798233060a6dc8c
usama-elojali <usama.elojali@sky.uk>
2025-07-08 16:43:10 +0100

    added EKS terraform block

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -1,3 +1,21 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-2"
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
+
